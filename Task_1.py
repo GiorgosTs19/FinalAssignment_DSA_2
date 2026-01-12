413fc51cfc1b274bff4c16891c22942698975d16
Giorgos <geontsou52@gmail.com>
2026-01-12 04:35:30 +0200

    Fixed a small bug, added success rate.

--- Task_1.py
+++ Task_1.py
@@ -39,40 +39,50 @@ def printBoard(board):
         print(row)
 
 # Generate a random order of the columns for a given row.
+# This is where the randomness is applied in my implementation.
+# The order in which I'm attempting to place a queen is random for every row.
 def generate_random_column_order(no_of_cols):
     return random.sample(range(no_of_cols), no_of_cols)
 
 def nQueensLasVegas(size: int) -> tuple[bool, list[list[int]]]:
-
-    failure = True
-    while failure:
-        # Assume that we haven't failed
-        failure = False
-        board = [[0 for _ in range(size)] for _ in range(size)]
-        for i in range(size):
-            col_order = generate_random_column_order(size)
-            print(f"Column order for row {i}: {col_order}")
-            row_success = False
-            for col in col_order:
-                print(f"Trying Col: {col} on row: {i}")
-                if is_safe(board, i, col):
-                    print(f"Col: {col} on row: {i} is safe")
-                    board[i][col] = 1
-                    row_success = True
-                    break
-            if not row_success:
-                # Trigger a failure, since this row hasn't been successful at placing a queen.
-                failure = True
+    board = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        col_order = generate_random_column_order(size)
+        print(f"Column order for row {i}: {col_order}")
+        row_success = False
+        for col in col_order:
+            print(f"Trying Col: {col} on row: {i}")
+            if is_safe(board, i, col):
+                print(f"Col: {col} on row: {i} is safe")
+                board[i][col] = 1
+                row_success = True
                 break
-    printBoard(board)
+        if not row_success:
+            return False, board
+    return True, board
 
 # Need to ask the user to input the board size (it's a square board)
 board_size = input("Enter board size: ")
 while not board_size.isnumeric() or int(board_size) <= 3:
     # Input validation, ensure it's a number
-    board_size = int(input("Enter a valid (number > 3) board size: "))
+    board_size = input("Enter a valid (number > 3) board size: ")
 approach = input("Select an approach (1 -> Las Vegas, 2 -> Backtracking): ")
 while not approach == "1" and not approach == "2":
-    approach = int(input("Select a valid approach (1 -> Las Vegas, 2 -> Backtracking): "))
+    approach = input("Select a valid approach (1 -> Las Vegas, 2 -> Backtracking): ")
 
-nQueensLasVegas(int(board_size))
+success, board = nQueensLasVegas(int(board_size))
+
+print(f"Success: {success}")
+printBoard(board)
+
+
+def calculate_success_rate(times:int) -> float:
+    successful_tries = 0
+    for i in range(times):
+        successful_tries, _ = nQueensLasVegas(5)
+        if successful_tries:
+            successful_tries += 1
+    
+    return successful_tries / times
+
+print(calculate_success_rate(10000))
